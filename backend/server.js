cf51c6949f884615c06a400c2b8f1af47c82170b
Ankan Dey <ankandey.02@gmail.com>
2024-08-13 18:23:51 +0530

    added connection pool

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const dbURL = `${process.env.MYSQL_URL}`;
-
-const db = mysql.createConnection(dbURL);
-
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err.stack);
-    return;
-  }
-  console.log("Connected to database.");
+const pool = mysql.createPool({
+  uri: process.env.MYSQL_URL, // or use individual options like host, user, password, database
+  connectionLimit: 10, // Adjust according to your needs
 });
 
 // Get banner details
 app.get("/api/banner", (req, res) => {
-  db.query("SELECT * FROM banner_db LIMIT 1", (err, result) => {
+  pool.query("SELECT * FROM banner_db LIMIT 1", (err, result) => {
     if (err) {
       console.log("query error...");
-      throw err;
+      res.status(500).json({ error: "Database query failed" });
+      return;
     }
     res.json(result[0]);
     console.log("query accepted, get");
@@ -33,13 +27,14 @@ app.get("/api/banner", (req, res) => {
 // Update banner details
 app.post("/api/banner", (req, res) => {
   const { description, timer, link, visible } = req.body;
-  db.query(
+  pool.query(
     "UPDATE banner_db SET description=?, timer=?, link=?, visible=? WHERE id=1",
     [description, timer, link, visible],
     (err, result) => {
       if (err) {
         console.log("cant post stuff here ...");
-        throw err;
+        res.status(500).json({ error: "Database query failed" });
+        return;
       }
       res.json({ status: "success" });
       console.log("query accepted, post");
